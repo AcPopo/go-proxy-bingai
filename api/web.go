ea23535c6b965ca893e3035d6e1e78bccb9d0174
AcPopo <139283385+AcPopo@users.noreply.github.com>
2023-07-20 17:48:25 +0800

    Update web.go

--- api/web.go
+++ api/web.go
@@ -1,9 +1,9 @@
 package api
 
 import (
-	"adams549659584/go-proxy-bingai/api/helper"
-	"adams549659584/go-proxy-bingai/common"
-	"adams549659584/go-proxy-bingai/web"
+	"AcPopo/go-proxy-bingai/api/helper"
+	"AcPopo/go-proxy-bingai/common"
+	"AcPopo/go-proxy-bingai/web"
 	"net/http"
 )
 
