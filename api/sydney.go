4e06c19de5aa82a3139da133a9180e3dca39777f
AcPopo <139283385+AcPopo@users.noreply.github.com>
2023-07-20 17:50:24 +0800

    Update sydney.go

--- api/sydney.go
+++ api/sydney.go
@@ -1,8 +1,8 @@
 package api
 
 import (
-	"adams549659584/go-proxy-bingai/api/helper"
-	"adams549659584/go-proxy-bingai/common"
+	"AcPopo/go-proxy-bingai/api/helper"
+	"AcPopo/go-proxy-bingai/common"
 	"net/http"
 )
 
